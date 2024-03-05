4495921f0b14ff79df8d3b4dcb34e0cdd7f22879
doron davidowitz <dorondavidowitz@gmail.com>
2024-03-05 18:26:45 +0200

    add API headers

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,14 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 //Init Telegram News Bot
 newsBot();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.FRONT_URL as string);
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get('/', async (req: Request, res: Response) => {
   try {
     res.json('hi');
